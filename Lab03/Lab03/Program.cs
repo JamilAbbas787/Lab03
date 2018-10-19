2b6a0d67e343a2058b815ed1e4cb11952695dd5f
Jamil Abbas <jamilabbas787@gmail.com>
2018-10-18 20:59:03 -0400

    First Upload

---
+++ Lab03/Lab03/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello! What is your name:   ");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine($"Welcome {userName}!");
+            Console.Write("Enter a number between 1 and 100:   ");
+            string userNumberEntry = Console.ReadLine();
+        
+            bool userNumberValidation
+        }
+    }
+}
