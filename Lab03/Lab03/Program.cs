ce5fd52242cf8495cf0e986fb7237df363c0b4e1
jakyle <jjackson360@gmail.com>
2018-10-22 20:35:53 -0400

    James TA Review

--- Lab03/Lab03/Program.cs
+++ Lab03/Lab03/Program.cs
@@ -16,23 +16,44 @@ namespace Lab03
             string userName = Console.ReadLine();
             Console.WriteLine($"Welcome {userName}!"); 
             string anotherOne;
+
+            // James - so the reasons most developers do not use do while loops is because 
+            // developers really value readability.  and from first glance, I'm not really sure where 
+            // this boolean is going to be used.  However because of your naming convention, it is 
+            // clear that this boolean is going to be used to repeat the loop.  so you solved that 
+            // problem by naming convention.  
+            //
+            // However, an alternative to breaking out of this loop is to just use 
+            // break;
+            // if the user did not enter "y"
+            // or use 
+            // continue;
+            // if the user did enter "y"
             bool repeatLoop;
 
             do
             {
                 Console.Write("Enter a number between 1 and 100:   ");
                 string userNumberEntry = Console.ReadLine();
+                // James - This is really cool that you are using a unsigned integer to make sure it's
+                // not a negative number, good stuff.  just as a tip though, I would use the C# type 
+                // instead of the dotnet type.  so you could use 
+                //
+                // uint.TryParse(...)
                 bool userNumberValidator = UInt32.TryParse(userNumberEntry, out UInt32 userNumber);
 
+                // James - is your number going to be less than 0 if it's an unsigned int ;) 
                 while (userNumber <= 0 || userNumber > 100)
                 {
                     Console.Write("Enter a number between 1 and 100:   ");
                     userNumberEntry = Console.ReadLine();
                     userNumberValidator = UInt32.TryParse(userNumberEntry, out userNumber);
                 }
-
+                // James - I like that your if statements are not nested, good job.
                 if (userNumber % 2 != 0)
                 {
+                    // James - good catch ;) 
+
                     //the instructions are repetive for odd number. Regardless of the odd number, it says to do the same thing
                     //so I used this for 
                     Console.WriteLine($"Hey {userName}, your number {userNumber} and odd.");
